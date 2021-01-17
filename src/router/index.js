89261d9456637eaa6a57f9ded2ea93a623eab897
borys25ol <oleynik.boris@gmail.com>
2021-01-17 19:39:59 +0200

    Mock all routes in projects.

--- src/router/index.js
+++ src/router/index.js
@@ -5,11 +5,6 @@ Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/',
-    name: 'globalFeed',
-    component: () => import('@/views/GlobalFeed'),
-  },
-  {
     path: '/register',
     name: 'register',
     component: () => import('@/views/Register'),
@@ -18,6 +13,51 @@ const routes = [
     path: '/login',
     name: 'login',
     component: () => import('@/views/Login'),
+  },
+  {
+    path: '/',
+    name: 'globalFeed',
+    component: () => import('@/views/GlobalFeed'),
+  },
+  {
+    path: '/feed',
+    name: 'yourFeed',
+    component: () => import('@/views/GlobalFeed'),
+  },
+  {
+    path: '/tags/:slug',
+    name: 'tag',
+    component: () => import('@/views/GlobalFeed'),
+  },
+  {
+    path: '/articles/new',
+    name: 'createArticle',
+    component: () => import('@/views/GlobalFeed'),
+  },
+  {
+    path: '/articles/:slug',
+    name: 'article',
+    component: () => import('@/views/GlobalFeed'),
+  },
+  {
+    path: '/articles/:slug/edit',
+    name: 'editArticle',
+    component: () => import('@/views/GlobalFeed'),
+  },
+  {
+    path: '/settings',
+    name: 'settings',
+    component: () => import('@/views/GlobalFeed'),
+  },
+  {
+    path: '/profiles/:slug',
+    name: 'userProfile',
+    component: () => import('@/views/GlobalFeed'),
+  },
+  {
+    path: '/profiles/:slug/favorites',
+    name: 'userProfileFavorites',
+    component: () => import('@/views/GlobalFeed'),
   },
 ]
 
